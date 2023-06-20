67d60f47b890d44c4161889e74879d3e523f7dc3
dapplion <35266934+dapplion@users.noreply.github.com>
2023-06-20 18:28:02 +0300

    Remove not necessary network data

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -4,7 +4,6 @@ exec node /usr/app/node_modules/.bin/lodestar \
     beacon \
     --preset=gnosis \
     --network=chiado \
-    --genesisStateFile=/usr/genesis.ssz \
     --jwt-secret=/jwtsecret \
     --execution.urls $HTTP_ENGINE \
     --eth1.providerUrls $HTTP_PROVIDER \
@@ -16,7 +15,6 @@ exec node /usr/app/node_modules/.bin/lodestar \
     --logFile /var/lib/data/beacon.log \
     --logFileLevel debug \
     --logFileDailyRotate 5 \
-    --bootnodes=enr:-LK4QN4NEavjPt7sJOfdcPKNucewHHxbtzdz5JCf_IRdsd58aIuix-9uIwSCc1TCfdi-j5HPunTxZ-TL10P4leQmGEEGh2F0dG5ldHOIAAAAAAAAAACEZXRoMpBI6hZ8AQAAb1oAAAAAAAAAgmlkgnY0gmlwhIuQGnOJc2VjcDI1NmsxoQJ7PqgatnROjTufoEKhwgeKxUJyLOacw0odaF1KS7F-7oN0Y3CCIyiDdWRwgiMo \
     $EXTRA_OPTS
 
 
